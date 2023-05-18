ef530d26b2e7d0ac4382cc0a9e4326403d34c5d7
Shubham Kc <shubhamkc1997@gmail.com>
2023-05-18 16:36:52 -0400

    Completed upto 12

--- exercises/e12.ts
+++ exercises/e12.ts
@@ -1,7 +1,20 @@
 // SPACE DATA EXERCISE 12
 // Return the sum of all moons for all planets
+
+import { AllData, Planet } from "../data/data";
+
 // Return example: 42
-export const allPlanetsMoonsCount = (data) => {};
+export const allPlanetsMoonsCount = (data: AllData) => {
+  return data.planets
+    .map((planet: Planet) => {
+      if (planet.moonsCount) {
+        return planet.moonsCount;
+      } else return 0;
+    })
+    .reduce((acc: number, prev: number) => {
+      return acc + prev;
+    });
+};
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-12"
